98a3044cf818e353ac7688244b5940aaf337eab6
otekolste <58699635+otekolste@users.noreply.github.com>
2024-08-29 17:13:34 -0400

    added signup feature

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,11 @@
-const loginFormHandler = async (event) => {
+const signupFormHandler = async (event) => {
   event.preventDefault();
 
   const username = document.querySelector("#username").value.trim();
   const password = document.querySelector("#password").value.trim();
 
   if (username && password) {
-    const response = await fetch("/api/users/login", {
+    const response = await fetch("/api/users", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
@@ -14,11 +14,11 @@ const loginFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace("/");
     } else {
-      alert("Failed to log in.");
+      alert("Failed to sign up.");
     }
   }
 };
 
 document
-  .querySelector("#loginButton")
-  .addEventListener("click", loginFormHandler);
+  .querySelector("#signUpButton")
+  .addEventListener("click", signupFormHandler);
