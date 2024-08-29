3a023a36b59a1789d807b0b64410a0f8773a3b20
otekolste <58699635+otekolste@users.noreply.github.com>
2024-08-29 13:44:30 -0400

    removed email from user model

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,12 +3,10 @@ const { User } = require("../models");
 const userData = [
   {
     username: "coolguy123",
-    email: "coolguy@gmail.com",
     password: "coolpassword",
   },
   {
     username: "awesomeguy123",
-    email: "awesomeguy@gmail.com",
     password: "awesomepassword",
   },
 ];
