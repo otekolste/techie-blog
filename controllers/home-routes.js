5cfd237e2cbcd061915bf67696000eb8cf9a78d3
otekolste <58699635+otekolste@users.noreply.github.com>
2024-08-29 13:07:27 -0400

    created template for blog posts and set homepage to render all posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Post, User } = require("../models");
 
-router.get('/', async(req, res) => {
-    res.render('homepage');
-})
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("homepage", {
+      posts,
+    });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
 
 module.exports = router;
-
