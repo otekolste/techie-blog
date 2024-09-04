374e27a884d0f378e66c5759877312b5822386e2
otekolste <58699635+otekolste@users.noreply.github.com>
2024-09-04 15:54:53 -0400

    added POST route for posts

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { Post } = require("../../models");
+
+// CREATE NEW COMMENT
+router.post("/", async (req, res) => {
+  try {
+    const dbPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.body.post_id,
+    });
+
+    req.session.save(() => {
+      res.status(200).json(dbPost);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
